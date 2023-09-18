e4eac2ad37cc7644cda6a705ee5ba2e84035d119
B4loncu <88264871+B4loncu@users.noreply.github.com>
2023-09-19 02:20:00 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Başarılı bir şekilde kayıt oldu :"+musteri.Id+musteri.Name+musteri.Age);
+        }
+        public void Remove(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri hesabı silindi :"+musteri.Id);
+        }
+        public void  Liste(Musteri musteri)
+        {
+            Console.WriteLine("Müsterilerin Bilgileri : "+musteri.Id + musteri.Name+musteri.Description+musteri.Age);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassMetotDemo;
+
+internal class Program
+{
+     static void Main(string[] args)
+    {
+        Musteri musteri1 = new Musteri();
+        musteri1.Id = "1000";
+        musteri1.Name = "Ahmat Muhsin";
+        musteri1.Description = "Arabic petrol rich!";
+        musteri1.Age = 30;
+        Musteri musteri2 = new Musteri();
+        musteri2.Id = "1001";
+        musteri2.Name = "Cenk Tosun";
+        musteri2.Description = "Futball player";
+        musteri2.Age = 27;
+        Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+
+        MusteriManager musteriManager = new MusteriManager();
+        musteriManager.Add(musteri1);
+        musteriManager.Remove(musteri2);
+        musteriManager.Liste(musteri1);
+        
+
+
+    }
+}
